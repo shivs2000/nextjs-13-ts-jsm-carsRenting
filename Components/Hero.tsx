fdc610f6569332a635704e5d767ef57de5d20103
SHIVAM@!! <43001208+shivs2000@users.noreply.github.com>
2023-08-14 22:41:45 +0530

    Setting Up

---
+++ Components/Hero.tsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, Rent or Book a Car -quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          Experience.
+        </p>
+      </div>
+    </div>
+  );
+};
