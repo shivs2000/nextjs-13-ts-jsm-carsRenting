7015c0aa8f5b9a3f3a475a31cd0eb30da52e0b17
SHIVAM@!! <43001208+shivs2000@users.noreply.github.com>
2023-08-15 10:01:22 +0530

    CustomButTon component

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import React from "react";
+import CustomButton from "./CustomButton";
+
 export const Hero = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -9,6 +15,11 @@ export const Hero = () => {
           Streamline your car rental experience with our effortless booking
           Experience.
         </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   );
