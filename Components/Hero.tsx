f2abed41dc71c57ff490fce89ae50f201aea9dfa
SHIVAM@!! <43001208+shivs2000@users.noreply.github.com>
2023-08-15 10:24:02 +0530

    hero image container

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import CustomButton from "./CustomButton";
+import Image from "next/image";
 
 export const Hero = () => {
   const handleScroll = () => {};
@@ -20,6 +21,12 @@ export const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+        <div className="hero__image-container">
+          <div className="hero__image">
+            <Image src="/hero.png" alt="hero" fill className="object-contain" />
+            <div className="hero__image-overlay"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
