91f1083b1f2876b43fa154b71b4cd5dee6c5c7b7
SHIVAM@!! <43001208+shivs2000@users.noreply.github.com>
2023-08-18 08:35:09 +0530

    added state and searchmanufacturer to searchbar

--- Components/SearchBar.tsx
+++ Components/SearchBar.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import { SearchManufacturer } from ".";
+
+import { useState } from "react";
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
